bccaca6f36e3be55565c6b08e57e957616bbfa28
unknown <mohamedeffatsayed@gmail.com>
2024-07-04 14:01:04 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import Weather from "./components/Weather.jsx";
+function App() {
+  return (
+    <div className="app">
+      <Weather />
+    </div>
+  );
+}
+export default App;
